d3ec168dd1f48abecd09732c526f87afb6c2f945
Anushkaraj1234 <rajanushka550@gmail.com>
2026-06-18 18:08:53 +0530

    Added DAY 15

---
+++ DAY 15/cd57.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int a[100],n,i;
+    printf("Enter the no. of elements");
+    scanf("%d",&n);
+    for(i = 0;i < n;i++)
+        scanf("%d",&a[i]);
+
+    for(i= n-1; i>= 0;i--)
+        printf("%d\n",a[i]);
+
+    return 0;
+}
