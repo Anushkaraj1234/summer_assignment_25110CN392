1d79a9be0c74073375fb01e5e50f4e30d1b5fb3d
Anushkaraj1234 <rajanushka550@gmail.com>
2026-06-20 21:33:15 +0530

    Added DAY 81 folder

---
+++ DAY 20/cd79.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10],n,i,j,f=1;
+
+    printf("Enter order of matrix: ");
+    scanf("%d", &n);
+
+    printf("Enter matrix elements:\n");
+    for (i = 0; i<n;i++)
+        for (j = 0; j < n; j++)
+            scanf("%d",&a[i][j]);
+
+    for (i=0;i<n;i++) {
+        for (j = 0; j < n; j++) {
+            if (a[i][j]!=a[j][i]) {
+                f= 0;
+                break;
+            }
+        }
+    }
+
+    if (f== 1)
+        printf("Matrix is symmetric");
+    else
+        printf("Matrix is not symmetric");
+
+    return 0;
+}
